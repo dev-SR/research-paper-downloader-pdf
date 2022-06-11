0f16ee81493074cad02c7fba1fa0f9a53f0b4ccc
Soikat Rahman <hello.dev.sr@gmail.com>
2022-06-12 02:41:50 +0600

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Research Article Downloader
+
+## Script
+
+```bash
+pipenv shell
+python download.py <n>
+# example:
+# python download.py 10
+```
+
+> `n`(optional): number of articles to download(default: 1) at each interval.
---
+++ download.py
@@ -0,0 +1,273 @@
+from datetime import datetime, timedelta
+import os
+from time import sleep
+# from rich.console import Console
+import pandas as pd
+from collections import deque
+from apscheduler.schedulers.background import BlockingScheduler
+from apscheduler.events import EVENT_JOB_EXECUTED, EVENT_JOB_ERROR
+from pytz import timezone
+
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+from requests.packages import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+from subprocess import check_output, STDOUT, CalledProcessError
+
+from rich.console import Console
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('n', nargs='?', const=0, type=int, default=1)
+args = parser.parse_args()
+console = Console()
+
+
+def handleGitCommit(m):
+    try:
+        check_output("git add .", shell=True)
+        s = check_output(f"git commit -m \"{m}\"", shell=True).decode()
+        return f"[green]Successfully Committed:[/green] {s}"
+    except CalledProcessError as e:
+        return f"[red]Error: {e.output.decode()}[/]"
+
+
+def handleGitPull():
+    try:
+        s = check_output("git pull origin main", shell=True).decode()
+        return f"[green]Pulled From Remote Repo:[/green] {s}"
+    except CalledProcessError as e:
+        return f"[red]Error: {e.output.decode()}[/]"
+
+
+def handleGitPush():
+    try:
+        s = check_output("git push origin main", shell=True).decode()
+        return f"[green]Pushed To Remote Repo,[/green] {s}"
+    except CalledProcessError as e:
+        return f"[red]Error: {e.output.decode()}[/]"
+
+
+def withLoader(cb, message="", spinner='aesthetic'):
+    done = False
+    returns = None
+    with console.status(f"[bold yellow] {message}...", spinner=spinner) as s:
+        while not done:
+            returns = cb()
+            done = True
+    return returns
+
+
+def withLoaderWithParam(cb, param, message="", spinner='aesthetic'):
+    done = False
+    returns = None
+    with console.status(f"[bold yellow] {message}...", spinner=spinner) as s:
+        while not done:
+            returns = cb(*param)
+            done = True
+    return returns
+
+
+def merge_dicts(*dict_args):
+    result = {}
+    for dictionary in dict_args:
+        result.update(dictionary)
+    return result
+
+
+# import ssl
+# import requests
+# from requests.adapters import HTTPAdapter
+# from urllib3.poolmanager import PoolManager
+# from urllib3.util import ssl_
+# CIPHERS = "ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-SHA256:AES256-SHA"
+# class TLSAdapter(HTTPAdapter):
+
+#     def __init__(self, ssl_options=0, *args, **kwargs):
+#         self.ssl_options = ssl_options
+#         super().__init__(*args, **kwargs)
+
+#     def init_poolmanager(self, *args, **kwargs):
+#         context = ssl_.create_urllib3_context(
+#             ciphers=CIPHERS, cert_reqs=ssl.CERT_REQUIRED, options=self.ssl_options)
+#         self.poolmanager = PoolManager(*args, ssl_context=context, **kwargs)
+
+
+# def download(url, fileName):
+#     headers = {
+#         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.53",
+#     }
+#     adapter = TLSAdapter(ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1)
+
+#     with requests.session() as session:
+#         # session.mount("https://www.researchgate.net/", adapter)
+#         session.mount('http://', adapter)
+#         session.mount('https://', adapter)
+
+#         response = session.get(url, headers=headers)
+#         # print(response.status_code)  # 200
+#         if response.status_code == 200:
+#             with open(f"data/papers_pdf/{fileName}.pdf", 'wb') as f:
+#                 f.write(response.content)
+#             return 0, 0
+#         else:
+#             return response.status_code, response.reason
+
+
+def downloadSS(url, fName):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.53",
+    }
+
+    session = requests.Session()
+    session = requests.Session()
+    retry = Retry(connect=3, backoff_factor=0.5)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    res = session.get(url, headers=headers, verify=False, timeout=30)
+    if res.status_code == 200:
+        with open(f"data/papers_pdf/{fName}.pdf", "wb") as f:
+            f.write(res.content)
+        session.close()
+        return 0, 0
+    else:
+        session.close()
+        return res.status_code, res.reason
+
+
+def downloadManager():
+    n = args.n
+    inf = pd.read_csv("data/info/papers.csv")
+    dl = pd.read_csv("data/info/processed.csv")
+    dl_dict = dl.to_dict('records')
+    uuids = [d['uuid'] for d in dl_dict]
+    paperIds = [d['paper_id'] for d in dl_dict]
+    next_dl = inf[~inf['uuid'].isin(uuids)]
+    next_dl_dict = next_dl.to_dict('records')[:n]
+    console.print(f"Downloading {n} papers")
+
+    for d in next_dl_dict:
+        paper_id = d['paper_id']
+        uuid = d['uuid']
+        download_links = d['download_link']
+        title = d['title']
+        links = download_links.split(";")
+        print()
+        success = False
+        errors = []
+        console.log(title)
+        if links[0] != "No links found":
+            console.log(f"[blue_violet]available links: {str(len(links))}[/]")
+
+        for i, link in enumerate(links):
+            if link == 'No links found':
+                nf = pd.DataFrame([d])
+                nf.to_csv("data/info/not_found.csv",
+                          index=False, header=False, mode="a")
+                pDf = pd.DataFrame([{
+                    'uuid': uuid,
+                    'paper_id': paper_id,
+                }])
+                pDf.to_csv("data/info/processed.csv",
+                           index=False, header=False, mode="a")
+                console.log(f"[yellow]No links available[/]")
+            else:
+                # already downloaded this paper before
+                if paper_id in paperIds:
+                    dlDf = pd.DataFrame([{
+                        'uuid': uuid,
+                        'paper_id': paper_id,
+                    }])
+                    dlDf.to_csv("data/info/processed.csv",
+                                index=False, header=False, mode="a")
+                    console.log("[green]already downloaded[/]")
+                    break
+                # download the paper
+                else:
+                    console.print(
+                        f"[blue1]-> Downloading with link[{i}]:[/] {link}")
+                    try:
+                        # code, reason = downloadSS(link, paper_id)
+                        code, reason = withLoaderWithParam(
+                            downloadSS, [link, paper_id], "Downloading...", 'dots')
+                        if code == 0:
+                            dlDf = pd.DataFrame([{
+                                'uuid': uuid,
+                                'paper_id': paper_id,
+                            }])
+                            dlDf.to_csv("data/info/processed.csv",
+                                        index=False, header=False, mode="a")
+                            console.log("[bright_green]downloaded[/]")
+                            success = True
+                            # no need to try other links
+                            break
+                        else:
+                            console.log(f"[red]Error:{code} {reason}[/]")
+                            errDict = merge_dicts(
+                                d, {'code': code, 'reason': reason})
+                            errors.append(errDict)
+
+                    except Exception as e:
+                        errDict = merge_dicts(
+                            d, {'code': None, 'reason': reason})
+                        errors.append(errDict)
+                        console.log(f"[red]Error:{e}[/]")
+        if not success:
+            dlDf = pd.DataFrame(errors)
+            dlDf.to_csv("data/info/errors.csv",
+                        index=False, header=False, mode="a")
+            console.log("[red]Failed to download[/]")
+
+
+# downloadManager()
+
+
+def startJob():
+    msg = withLoader(handleGitPull, message="Pulling from remote repo")
+    console.log(msg)
+
+    downloaded = pd.read_csv("data/info/processed.csv")
+    preLen = len(downloaded)
+
+    sched = BlockingScheduler(timezone=timezone('Asia/Dhaka'))
+
+    sched.add_job(downloadManager, 'interval',
+                  minutes=5,
+                  #   seconds=2,
+                  next_run_time=datetime.now(),  # start immediately
+                  end_date=datetime.now() + timedelta(minutes=2),
+                  id='my_job_id')
+
+    def execution_listener(event):
+        if event.exception:
+            print('The job crashed')
+            return
+
+        # console.log('The job executed successfully')
+        job = sched.get_job(event.job_id)
+        print()
+        try:
+            if job.id == 'my_job_id':
+                console.log(
+                    f"[yellow1]Next Job scheduled to be run at: {job.next_run_time}[/]")
+                # check if there is any paper in the queu
+
+        except:
+            # all the jobs are done
+            downloaded = pd.read_csv("data/info/processed.csv")
+            postLen = len(downloaded)
+            handleGitCommit(f"{(postLen - preLen)} papers downloaded")
+            msg = withLoader(
+                handleGitPush, message="Pushing to the remote repo")
+            console.log(msg)
+            sched.shutdown(wait=False)
+
+    sched.print_jobs()
+    sched.add_listener(execution_listener,
+                       EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
+    sched.start()
+
+
+startJob()
